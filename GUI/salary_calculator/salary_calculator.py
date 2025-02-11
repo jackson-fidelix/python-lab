9aa90de9fc535c75fc7f87ba98d346149aa4407b
Jackson Fidelix <jcksfidelix@gmail.com>
2025-02-11 18:16:24 -0300

    Create Salary-Calculator

---
+++ GUI/salary_calculator/salary_calculator.py
@@ -0,0 +1,35 @@
+from PySimpleGUI import PySimpleGUI as sg
+
+#Layout
+sg.theme('Kayak')
+layout = [
+    [sg.Text('Salário Bruto:'), sg.Input(key='salario_bruto',size=(13,2))],
+    [sg.Text('Benefícios:'), sg.Input(key='beneficios',size=(16,2))],
+    [sg.Text('Outros Descontos:'), sg.Input(key='outros_descontos',size=(10,2))],
+    [sg.Text('')],# para pular uma linha
+    [sg.Button('Calcular')],
+    [sg.Text('')],# para pular uma linha
+    [sg.Text('Salário Liquido:'), sg.Input('', size=(13,2),key='salario_liquido')]
+]
+
+#Janela
+janela = sg.Window('Calculadora de Salário Líquido', layout, element_justification='center')
+
+#Ler os eventos
+while True:
+    eventos, valores = janela.read()
+    if eventos == sg.WIN_CLOSED:
+        break
+    # condição de calculo
+    if eventos == "Calcular":
+        entrada = valores["salario_bruto"].strip()
+        if entrada:
+            salario_liquido = float(valores['salario_bruto'])
+            janela['salario_liquido'].update(f'R$ {salario_liquido:.2f}')
+        entrada_beneficios = valores["beneficios"].strip()
+        if entrada_beneficios:
+            beneficios = float(valores["beneficios"])
+            salario_liquido += beneficios
+            janela['salario_liquido'].update(f'R$ {salario_liquido:.2f}')
+        print('OK')
+janela.close()
---
+++ README.md
@@ -0,0 +1,23 @@
+# Python Projects 🚀  
+
+Este repositório contém diversos projetos e experimentos desenvolvidos em **Python**, incluindo interfaces gráficas, automação de planilhas e scripts úteis.  
+
+## 📂 Estrutura  
+- **GUI/** → Projetos com interface gráfica (PySimpleGUI, Tkinter, PyQt).  
+- **DataProcessing/** → Automação e manipulação de dados (Excel, CSV, Pandas).  
+- **Scripts/** → Pequenos utilitários para tarefas diversas.  
+
+## 🛠 Tecnologias  
+- Python 3
+- PySimpleGUI  
+
+
+## 📌 Exemplos de Projetos  
+### 🔹 **Salary Calculator (GUI)**  
+Um aplicativo simples feito com **PySimpleGUI** para calcular o salário líquido com base em descontos e impostos.  
+
+📍 **Local:** `GUI/salary_calculator/`  
+📜 **Arquivo principal:** `salary_calculator.py`  
+
+---
+🚀 **Mais projetos serão adicionados conforme meu aprendizado em Python evolui!**  
