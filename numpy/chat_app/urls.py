75f17a8b57b5262784730a4bd77e9cacf695bb3d
Jackson Fidelix <116315846+jackson-fidelix@users.noreply.github.com>
2025-12-15 21:38:33 -0300

    Delete numpy directory

--- numpy/chat_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('', views.chat, name='chat'),
-    path('process/', views.process, name='process')
-]
--- numpy/pokemon_chat/wsgi.py
+++
@@ -1,6 +0,0 @@
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pokemon_chat.settings')
-
-application = get_wsgi_application()
