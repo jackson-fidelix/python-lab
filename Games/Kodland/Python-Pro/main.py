311532a17cdaf413d358f4803cd4dd94f49da165
Jackson Fidelix <116315846+jackson-fidelix@users.noreply.github.com>
2026-06-15 18:15:32 -0300

    Update main.py

--- Games/Kodland/Python-Pro/main.py
+++ Games/Kodland/Python-Pro/main.py
@@ -1,16 +1,41 @@
-print("**"*25)
-print("Tarefa M1L1 - T3 (resolvendo traduções modernas)")
-print("**"*25)
+import discord
+from discord.ext import commands
+import random
 
-word = input('Digite uma palavra que você não entende: ').upper()
+description = '''An example bot to showcase the discord.ext.commands extension
+module.'''
 
-meme_dict = {
-    "BETINHA": "O cara que fica moscando...",
-    "MOSCAR": "o cara que é desligado, não tem atenção",
-    "SIGMA": "Uma pessoa de respeito!"
-}
+# A variável intents armazena as permissões do bot
+intents = discord.Intents.default()
+# Ativar a permissão para ler o conteúdo das mensagens
+intents.message_content = True
+# Criar um bot e passar as permissões
+bot = commands.Bot(command_prefix='?', description=description, intents=intents)
 
-if word in meme_dict:
-    print(meme_dict[word])
-else:
-    print("Infelizmente nem nós sabemos essa! kkkkkk")
+@bot.event
+async def on_ready():
+    print('------'*10)
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------'*10)
+
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello!")
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+bot.run('Insira seu token')
